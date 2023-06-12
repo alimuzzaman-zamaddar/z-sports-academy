b698ddfcc46f23657acc3835fee953de7f06f8ad
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-06-12 11:45:54 +0600

    done

--- src/Home/PopularClasses/PopularClasses.jsx
+++ src/Home/PopularClasses/PopularClasses.jsx
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "./PopularClasses.css";
+import useAllClasses from "../../Hooks/useAllClasses";
 import PopularCCard from "./PopularCCard";
 
 const PopularClasses = () => {
   const [pClass,setPClass] = useState([])
   useEffect(() => {
 
-    fetch('https://zamaddar-sports-club-server.vercel.app/class')
+    fetch('https://zamaddar-sports-club-server.vercel.app/popularClass')
     .then(res => res.json())
     .then(data => setPClass(data))
+
   },[])
-    const popularClasses = pClass.filter((aclass) => aclass.enrolled > 0);
+
   return (
     <>
     <h1 className="text-center text-7xl font-extrabold py-8 font-serif">POPULAR CLASSES SECTION</h1>
     <p className="text-center py-4">most popular classes are here and the best trainers are waiting for you </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-11">
-        {popularClasses.map((pClass) => (
+        {pClass.map((pClass) => (
           <PopularCCard key={pClass._id} pClass={pClass}></PopularCCard>
         ))}
       </div>
